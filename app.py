d3110e8f7803c2ae048cd368bd8fe3725590d0ba
Joe king <105607242+JoesphK@users.noreply.github.com>
2026-01-12 02:19:07 +0200

    Attempt to repair the project

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import os  # Import the os module
 
 app = Flask(__name__)
 
@@ -12,12 +13,12 @@ app.config['MYSQL_USER'] = 'Youssef'
 app.config['MYSQL_PASSWORD'] = 'GBG_Acadmey!'
 app.config['MYSQL_DB'] = 'AZSQLDB'
 app.config['PORT'] = 1433
-app.config['MYSQL_SSL_CA'] = 'DigiCertGlobalRootG2.crt.pem'
+app.config['MYSQL_SSL_CA'] = os.path.join(os.path.dirname(__file__), 'DigiCertGlobalRootG2.crt.pem')  # Resolve path relative to app.py
 app.config['MYSQL_SQL_ENCRYPT'] = 'optional'
 app.config['MYSQL_SQL_SSL_VERIFY_SERVER_CERT'] = False
 
-
 mysql = MySQL(app)
+
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
@@ -39,12 +40,14 @@ def login():
             msg = 'Incorrect username/password!'
     return render_template('login.html', msg=msg)
 
+
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
     return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -72,11 +75,13 @@ def register():
         msg = 'Please fill out the form!'
     return render_template('register.html', msg=msg)
 
+
 @app.route('/home')
 def home():
     if 'loggedin' in session:
         return render_template('index.html', username=session['username'])
     return redirect(url_for('login'))
+
 
 @app.route('/profile')
 def profile():
@@ -87,6 +92,7 @@ def profile():
         return render_template('profile.html', account=account)
     return redirect(url_for('login'))
 
+
 @app.route('/users')
 def users():
     if 'loggedin' in session:
@@ -95,6 +101,7 @@ def users():
         users = cursor.fetchall()
         return render_template('users.html', users=users)
     return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
